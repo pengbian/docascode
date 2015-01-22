3b2b2436e8be4066e9e6d4da845334f687dbd996
Shuang Jiang <t-shujia@microsoft.com>
2015-01-22 10:06:50 +0800

    modify css

--- GenDoc/MergeDoc/Templates/script/default-ios.js
+++ GenDoc/MergeDoc/Templates/script/default-ios.js
@@ -3,7 +3,7 @@ function init() {
 
 
 $(document).ready(function () {
-    $("article li.section-member a.section-member-name").click(function () {
+    $("article li.section-member a.section-member-detail").click(function () {
         $(this).next("article div.height-container").animate({ 'height': 'toggle' }, 'normal');
     });
 });
