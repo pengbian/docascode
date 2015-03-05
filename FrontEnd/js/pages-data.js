9be0aad840d05fe8544dcebd938013270d34e4a0
unknown <lianwei@lianwei-laptop.fareast.corp.microsoft.com>
2015-03-05 15:36:03 +0800

    Add frontend

---
+++ FrontEnd/js/pages-data.js
@@ -0,0 +1,28 @@
+// Meta data used by the AngularJS docs app
+angular.module('pagesData', [])
+  .value('NG_PAGES', {
+  "api": {
+    "name": "API Reference",
+    "area": "api",
+    "type": "toc",
+    "path": "api"
+  },
+  "api/namespace1": {
+    "name": "Namespace 1",
+    "type": "namespace",
+    "area": "api",
+    "path": "api/namespace1"
+  },
+  "api/namespace2": {
+    "name": "Namespace 2",
+    "area": "api",
+    "type": "namespace",
+    "path": "api/namespace2"
+  },
+  "api/class1": {
+    "name": "Class 1",
+    "area": "api",
+    "type": "class",
+    "path": "api/class1"
+  },
+});
