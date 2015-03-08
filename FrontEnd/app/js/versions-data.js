56d8fc9453ab0b7f5b7f2f17a4b6fdf6907c4b14
unknown <lianwei@lianwei-laptop.fareast.corp.microsoft.com>
2015-03-08 22:25:13 +0800

    Import Frontend --start

---
+++ FrontEnd/app/js/versions-data.js
@@ -0,0 +1,34 @@
+// Meta data used by the AngularJS docs app
+angular.module('versionsData', [])
+  .value('NG_VERSION', {
+  "raw": "v1.4.0-beta.5",
+  "major": 1,
+  "minor": 4,
+  "patch": 0,
+  "prerelease": [
+    "local"
+  ],
+  "build": "sha.8200011",
+  "version": "1.4.1-local",
+  "codeName": "snapshot",
+  "isSnapshot": true,
+  "full": "1.4.0-local+sha.8200011",
+  "branch": "master"
+})
+  .value('NG_VERSIONS', [
+  {
+    "raw": "v1.4.0-beta.5",
+    "major": 1,
+    "minor": 4,
+    "patch": 0,
+    "prerelease": [
+      "local"
+    ],
+    "build": "sha.8200011",
+    "version": "1.4.0-local",
+    "codeName": "snapshot",
+    "isSnapshot": true,
+    "full": "1.4.0-local+sha.8200011",
+    "branch": "master"
+  }
+]);
