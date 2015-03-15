5d54c48f152da4d9e42ea64f08fab424a2ea6612
Liangying Wei <lianwei@microsoft.com>
2015-03-15 12:01:26 +0800

    Improve BuildMeta Commandline helper text

--- GenDoc/BuildMeta/Program.cs
+++ GenDoc/BuildMeta/Program.cs
@@ -1,21 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Symbols;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Text;
 using System.IO;
-using Microsoft.CodeAnalysis.MSBuild;
-using Newtonsoft.Json;
-using System.Collections;
 using DocAsCode.Utility;
-using System.ComponentModel;
 using EntityModel;
-using System.Threading.Tasks;
-using System.Collections.Concurrent;
 
 namespace DocAsCode.BuildMeta
 {
@@ -37,28 +23,28 @@ namespace DocAsCode.BuildMeta
             const string DefaultMdFolderName = "md";
             const string DefaultApiFolderName = "api";
             const string DefaultOutputFolderName = "output";
-            string projectListFile = null;
+            string inputProjectList = null;
             string outputFolder = null;
             string outputListFile = null;
-            string markdownListFile = null;
-            string markdownIndexFileName = null;
-            string tocFileName = null;
-            string indexFileName = null;
-            string apiFolderName = null;
-            string mdFolderName = null;
+            string inputMarkdownList = null;
+            string outputMarkdownIndexFile = null;
+            string outputTocFile = null;
+            string outputIndexFile = null;
+            string outputApiFolder = null;
+            string outputMarkdownFolder = null;
 
             try
             {
                 var options = new Option[]
                     {
-                    new Option(null, p => projectListFile = p, helpName: "projectListFile", required: false, helpText: "Required. Specify [1. the file path with extension .list | 2. the file list seperated by comma(,)] that contains all the project/solution paths whose metadata is to be generated."),
-                    new Option("m", s => markdownListFile = s, defaultValue: null, helpName: "markdownListFile", helpText: "Specify the file path that contains all the additional markdown files. (default: null)."),
+                    new Option(null, p => inputProjectList = p, helpName: "inputProjectList", required: false, helpText: "Required. Specify the list of project files to generate documentation. Supported input types are: [1. the file path with extension .list | 2. the file paths seperated by comma(,)]."),
+                    new Option("m", s => inputMarkdownList = s, defaultValue: null, helpName: "inputMarkdownList", helpText: "Specify the list of markdown files that contains all the additional documentations to the members in projects files. Supported input types are: [1. the file path with extension .list | 2. the file paths seperated by comma(,)]."),
                     new Option("o", s => outputFolder = s, defaultValue: DefaultOutputFolderName, helpName: "outputFolder", helpText: "Specify the output folder that contains all the generated metadata files. (default: " + DefaultOutputFolderName + ")."),
-                    new Option("toc", s => tocFileName = s, defaultValue: DefaultTocFileName, helpName: "tocFileName", helpText: "Specify the file name containing all the namespace yaml file paths. (default: " + DefaultTocFileName + ")."),
-                    new Option("index", s => indexFileName = s, defaultValue: DefaultIndexFileName, helpName: "indexFileName", helpText: "Specify the file name containing all the available yaml files. (default: " + DefaultIndexFileName + ")."),
-                    new Option("md", s => markdownIndexFileName = s, defaultValue: DefaultMarkdownFileName, helpName: "markdownIndexFileName", helpText: "Specify the file name containing all the markdown index. (default: " + DefaultMarkdownFileName + ")."),
-                    new Option("folder", s => apiFolderName = s, defaultValue: DefaultApiFolderName, helpName: "apiFolderName", helpText: "Specify the subfolder name containing all the member's yaml file. (default: " + DefaultApiFolderName + ")."),
-                    new Option("mdFolder", s => mdFolderName = s, defaultValue: DefaultMdFolderName, helpName: "mdFolderName", helpText: "Specify the subfolder name containing all the markdown files copied. (default: " + DefaultMdFolderName+ ")."),
+                    new Option("toc", s => outputTocFile = s, defaultValue: DefaultTocFileName, helpName: "outputTocFile", helpText: "Specify the file name containing all the namespace yaml file paths. (default: " + DefaultTocFileName + ")."),
+                    new Option("index", s => outputIndexFile = s, defaultValue: DefaultIndexFileName, helpName: "outputIndexFile", helpText: "Specify the file name containing all the available yaml files. (default: " + DefaultIndexFileName + ")."),
+                    new Option("md", s => outputMarkdownIndexFile = s, defaultValue: DefaultMarkdownFileName, helpName: "outputMarkdownIndexFile", helpText: "Specify the file name containing all the markdown index. (default: " + DefaultMarkdownFileName + ")."),
+                    new Option("folder", s => outputApiFolder = s, defaultValue: DefaultApiFolderName, helpName: "outputApiFolder", helpText: "Specify the output subfolder name containing all the member's yaml file. (default: " + DefaultApiFolderName + ")."),
+                    new Option("mdFolder", s => outputMarkdownFolder = s, defaultValue: DefaultMdFolderName, helpName: "outputMarkdownFolder", helpText: "Specify the output subfolder name containing all the markdown files copied. (default: " + DefaultMdFolderName+ ")."),
                     };
 
                 if (!ConsoleParameterParser.ParseParameters(options, args))
@@ -66,21 +52,21 @@ namespace DocAsCode.BuildMeta
                     return 1;
                 }
 
-                if (projectListFile == null || projectListFile.Length == 0)
+                if (inputProjectList == null || inputProjectList.Length == 0)
                 {
                     ParseResult.WriteToConsole(ResultLevel.Warn, "No source project files are referenced. No documentation will be generated.");
                     return 2;
                 }
 
                 outputListFile = Path.GetRandomFileName() + ".list";
-                BuildMetaHelper.GenerateMetadataFromProjectListAsync(projectListFile, outputListFile).Wait();
-                BuildMetaHelper.MergeMetadataFromMetadataListAsync(outputListFile, outputFolder, indexFileName, BuildMetaHelper.MetadataType.Yaml).Wait();
-                BuildMetaHelper.GenerateIndexForMarkdownListAsync(outputFolder, indexFileName, markdownListFile, markdownIndexFileName, mdFolderName).Wait();
+                BuildMetaHelper.GenerateMetadataFromProjectListAsync(inputProjectList, outputListFile).Wait();
+                BuildMetaHelper.MergeMetadataFromMetadataListAsync(outputListFile, outputFolder, outputIndexFile, BuildMetaHelper.MetadataType.Yaml).Wait();
+                BuildMetaHelper.GenerateIndexForMarkdownListAsync(outputFolder, outputIndexFile, inputMarkdownList, outputMarkdownIndexFile, outputMarkdownFolder).Wait();
                 return 0;
             }
             catch (Exception e)
             {
-                Console.Error.WriteLine("Failing in generating metadata from {0}: {1}", projectListFile, e);
+                Console.Error.WriteLine("Failing in generating metadata from {0}: {1}", inputProjectList, e);
                 return 1;
             }
         }
